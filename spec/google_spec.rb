057f6274892c4af883339d99130eb225c51434ae
Peter MacRobert <originalpete@gmail.com>
2013-10-24 16:58:36 +0100

    working demo of rspec + capybara

--- spec/google_spec.rb
+++ spec/google_spec.rb
@@ -1,6 +1,42 @@
-describe "Google.co.uk" do
-  it "contains a search box" do
-    expect(1).to eq(1)
+describe "google.co.uk",  :type => :feature do
+  
+  context "the page" do
+  
+    before do
+      visit "/"
+    end
+  
+    it "contains a search button" do
+      expect(page).to have_button "Google Search"
+    end
+  
+    it "has feeling lucky button" do
+      expect(page).to have_button "I'm Feeling Lucky"
+    end
+  
+    it "has a search box" do
+      expect(page).to have_field('q', :type => 'text')
+    end
+    
+  end
+  
+  context "doing a search for pixie labs on twitter" do
+    before do
+      visit "/"
+      fill_in 'q', :with => 'pixie labs twitter'
+      
+      # no button to click now that google auto-starts the search
+      # click_button "Google Search"
+    end
+    
+    it "should return search results" do
+      expect(page).to have_text "results"
+    end
+    
+    it "should list the pixielabs twitter page" do
+      expect(page).to have_text "Pixie Labs (pixielabs) on Twitter"
+    end
+    
   end
   
 end
